650c56021066299cd266f5038f16e22b1dcc4013
Tim Park <timfpark@gmail.com>
2022-12-15 07:53:03 -0800

    Add test of /health endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use axum::{response::Html, routing::get, Router};
-use std::net::SocketAddr;
+use std::env;
+
+async fn health() -> Html<&'static str> {
+    Html("ok")
+}
+
+pub fn http_router() -> Router {
+    Router::new().route("/health", get(health))
+}
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/health", get(health));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+async fn main() -> anyhow::Result<()> {
+    let app = http_router();
+    let endpoint = env::var("ENDPOINT").unwrap_or_else(|_| "0.0.0.0:8080".to_owned());
+    let addr = endpoint.parse()?;
 
     println!("listening on {}", addr);
 
@@ -12,8 +21,38 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
 
-async fn health() -> Html<&'static str> {
-    Html("ok")
+#[cfg(test)]
+mod tests {
+    use axum::{
+        body::Body,
+        http::{self, StatusCode},
+    };
+    use tower::ServiceExt;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_health() {
+        let app = http_router();
+
+        let response = app
+            .oneshot(
+                http::Request::builder()
+                    .method(http::Method::GET)
+                    .uri("/health")
+                    .body(Body::empty())
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(&body[..], b"ok");
+    }
 }
